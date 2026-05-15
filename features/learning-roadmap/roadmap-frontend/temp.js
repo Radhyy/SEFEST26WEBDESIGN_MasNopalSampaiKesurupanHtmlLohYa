4258592129324abeb58ce21df2456d6b437f06ec
Radhyy <raaakb87@gmail.com>
2026-05-15 15:08:33 +0700

    update

---
+++ features/learning-roadmap/roadmap-frontend/temp.js
@@ -0,0 +1,21 @@
+
+    tailwind.config = {
+      darkMode: "class",
+      theme: {
+        extend: {
+          fontFamily: {
+            sans: ["Plus Jakarta Sans", "sans-serif"],
+            display: ["Sora", "sans-serif"],
+          },
+          colors: {
+            brand: {
+              50: "#eef3ff",
+              100: "#dbe5ff",
+              500: "#2f5bd3",
+              600: "#1d43b4",
+            },
+          },
+        },
+      },
+    };
+  
